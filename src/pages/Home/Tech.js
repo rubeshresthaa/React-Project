b711d116b09428aaa976af08870d9903e536abc7
rubeshresthaa <rubesh.shrestha619@gmail.com>
2024-07-03 10:09:28 +0545

    Hover

--- src/pages/Home/Tech.js
+++ src/pages/Home/Tech.js
@@ -12,14 +12,14 @@ import { FaAngular } from "react-icons/fa";
 const Tech = () => {
   return (
     <div>
-      <h1  className="text-center font-bold	text-2xl mb-5">Technology I Use</h1>
-      <div className="devicons grid grid-cols-4 justify-items-center	gap-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-      <FaReact size={100} className="hover:size-[200px] hover:text-red-400"/>
-      <FaJava size={100}/>
-      <FaVuejs size={100}/>
-      <FaCss3 size={100} className="animate-bounce"/>
-      <FaPython size={100}/>
-      <FaAngular size={100}/>
+      <h1  className="text-center font-bold	text-2xl mb-5 sm:text-lg sm-font-bold">Technology I Use</h1>
+      <div className="devicons grid grid-cols-4 justify-items-center	gap-10 cursor-pointer lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      <FaReact size={100} className="hover:scale-125	hover:text-red-400 "/>
+      <FaJava size={100} className="hover:scale-125	hover:text-red-400"/>
+      <FaVuejs size={100} className="hover:scale-125	hover:text-red-400"/>
+      <FaCss3 size={100} className="animate-bounce hover:scale-125 hover:text-red-400"/>
+      <FaPython size={100} className="hover:scale-125	hover:text-red-400"/>
+      <FaAngular size={100} className="hover:scale-125	hover:text-red-400"/>
 
 
 
